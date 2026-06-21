f90d60ca7c22170a8e68bdcfbbb1bd265147b2af
Arsyi Aditama <altschmerzsiuu@gmail.com>
2026-06-22 01:47:08 +0800

    chore: configure github pages build and deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/bintikbumi-web/',
   assetsInclude: ['**/*.MP4', '**/*.mov', '**/*.MOV']
 })
